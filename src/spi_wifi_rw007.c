5962b69717d9c3cf0417797ab4c53518b216e451
zyh <lymz@foxmail.com>
2019-01-30 11:40:23 +0800

    修改最大时钟为30M

--- src/spi_wifi_rw007.c
+++ src/spi_wifi_rw007.c
@@ -560,7 +560,7 @@ rt_err_t rt_hw_wifi_init(const char *spi_device_name, wifi_mode_t mode)
         struct rt_spi_configuration cfg;
         cfg.data_width = 8;
         cfg.mode = RT_SPI_MODE_0 | RT_SPI_MSB; /* SPI Compatible: Mode 0. */
-        cfg.max_hz = 15 * 1000000;             /* 15M 007 max 30M */
+        cfg.max_hz = 30 * 1000000;             /* max 30M */
         rt_spi_configure(rw007_wifi_device.rt_spi_device, &cfg);
     }
 
